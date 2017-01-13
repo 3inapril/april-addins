caa46b2c1e772a4ed32f3440a268423d86add0dd
April Liu <xiaoshan.liu@dominionenterprises.com>
2017-01-13 13:18:22 -0500

    fix range error

--- AprilAddins/R/comment_uncomment_chunk.R
+++ AprilAddins/R/comment_uncomment_chunk.R
@@ -1,6 +1,6 @@
-#' Comment out the highlighted code chunk
-#'
-#' Call this as an addin to comment out selected rows by inserting a pound # 
+  ' Comment out the highlighted code chunk
+ # 
+ #' Call this as an addin to comment out selected rows by inserting a pound # 
 #' in front of the row
 #'
 #' @export
@@ -29,22 +29,20 @@ uncomment_chunk_addin <- function() {
   context <- rstudioapi::getActiveDocumentContext()
   
   start_row <- context$selection[[1]]$range$start[1]
-  #start_col <- context$selection[[1]]$range$start[2]
+  start_col <- context$selection[[1]]$range$start[2]
   end_row <- context$selection[[1]]$range$end[1]
-  #end_col <- context$selection[[1]]$range$end[2]
+  row_seq <- start_row:end_row
+  col_seq <- c(start_col-1, rep(0, end_row-start_row))
   
   sel_text <- context$selection[[1]]$text
   row_str <- strsplit(sel_text, '\n')
   
-  # which one has leading pond
-  idx <- which(substr(trimws(row_str[[1]]), 1, 1) == '#')
-  pos <- regexpr("#", row_str[[1]])
+  # which one has leading pound
+  rltv_idx <- which(substr(trimws(row_str[[1]]), 1, 1) == '#')
+  rows <- row_seq[rltv_idx]
+  cols <- as.numeric(col_seq+regexpr("#", row_str[[1]]))[rltv_idx]
   
-  for (i in idx){
-    
-    row <- (start_row:end_row)[i]
-    p <- pos[idx]
-    rstudioapi::modifyRange(c(row, p), "")
-  }
+  rng <- Map(c, Map(c, rows, cols), Map(c, rows, cols+1))
+  rstudioapi::modifyRange(rng, " ")
  
 }
